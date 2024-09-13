959d9564aa2cbba37c0e79d79020761c07ecf101
Omer Szerer <omerszerer@gmail.com>
2024-09-13 15:03:34 +0200

    Add a memes folder

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import axios from 'axios'; //imports a library for making HTTP requests
-import * as cheerio from 'cheerio'; //imports a library for manipulating HTML, select all images
-import fs from 'fs'; //imports a node.js module for interacting with the file system
+import fs from 'node:fs'; // imports a node.js module for interacting with the file system
+import axios from 'axios'; // imports a library for making HTTP requests
+import * as cheerio from 'cheerio'; // imports a library for manipulating HTML, select all images
 
-const url = 'https://memegen-link-examples-upleveled.netlify.app/'; //the desired website
+if (!fs.existsSync("memes")) { // creates a "meme" directory
+  fs.mkdirSync("memes");
+}
+
+const url = 'https://memegen-link-examples-upleveled.netlify.app/'; // the desired website
 
 const response = await axios.get(url); // a variable that await the response
 const $ = cheerio.load(response.data); // a variable that loads the html content
@@ -11,10 +15,6 @@ const imageSrc = $('img') // return the first 10 images with src attribute
   .map((i, element) => $(element).attr('src'))
   .get()
   .slice(0, 10);
-
-if (!fs.existsSync("memes")) { // creates a "meme" folder
-  fs.mkdirSync("memes");
-}
 
 
 console.log(imageSrc)
